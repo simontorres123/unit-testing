3ce443981dfec3f24d377755f60a26e4187af691
AdanAlvarezV <35908086+AdanAlvarezV@users.noreply.github.com>
2022-11-01 23:54:17 -0600

    Change service annotation
    Service annotation removed on IPersonService
    Service annotation added on PersonServiceImpl

--- src/main/java/com/demo/unittesting/service/PersonServiceImpl.java
+++ src/main/java/com/demo/unittesting/service/PersonServiceImpl.java
@@ -3,9 +3,11 @@ package com.demo.unittesting.service;
 import com.demo.unittesting.domain.Person;
 import com.demo.unittesting.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PersonServiceImpl implements IPersonService{
 
     @Autowired
